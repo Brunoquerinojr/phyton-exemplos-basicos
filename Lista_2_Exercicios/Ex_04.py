93dd8cb51ccb112cae7cb5431ca53988eafb6fe5
Brunoquerinojr <brunoquerinosilvajr@gmail.com>
2024-08-08 15:29:00 -0300

    attex4

---
+++ Lista_2_Exercicios/Ex_04.py
@@ -0,0 +1,9 @@
+# Variaveis
+nome = input("Informe o seu nome: ")
+nota = int(input("Informe a sua nota: "))
+
+# Verifica a idade
+if nota >=5:
+    print(f"Olá; {nome}! Você está {nota} aprovado!")
+else:
+    print(f"Olá; {nome}! Você {nota} está reprovado")
