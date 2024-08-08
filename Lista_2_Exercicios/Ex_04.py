8fcbb2634f17f88ef83f7c07df714a4b48b687b8
Brunoquerinojr <brunoquerinosilvajr@gmail.com>
2024-08-08 15:46:39 -0300

    attex4

--- Lista_2_Exercicios/Ex_04.py
+++ Lista_2_Exercicios/Ex_04.py
@@ -1,9 +1,7 @@
-# Variaveis
-nome = input("Informe o seu nome: ")
 nota = int(input("Informe a sua nota: "))
 
-# Verifica a idade
-if nota >=5:
-    print(f"Olá; {nome}! Você está {nota} aprovado!")
-else:
-    print(f"Olá; {nome}! Você {nota} está reprovado")
+# Uso do If ternário
+alerta = "Você está aprovado" if nota > 5 else "Você está reprovado"
+
+# Exibição
+print(alerta)
